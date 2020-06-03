3231eec2ecb2f9a2cfca6362b84c94b662c9e9f0
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-03 09:10:16 -0300

    ajust package.json

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -8,7 +8,6 @@ class File extends Model {
       url: {
         type: Sequelize.VIRTUAL,
         get() {
-          // return `http://localhost:3333/files/${this.path}`
           return `http://www.gotattoo.app/files/${this.path}`
         }
       }
