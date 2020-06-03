e4569606f210ae2b256178de31fd9d3e69009a74
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-03 19:14:25 -0300

    digital ocean test

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app';
 
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-app.listen(3333);
+app.listen(3333, () => { console.log ('Server Started.')});
 // app.listen(3333);
