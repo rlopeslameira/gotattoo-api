98dce9efe4e30ce684be60f56f83d82995520e8f
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-04 09:42:26 -0300

    ip

--- dist/config/auth.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  secret: 'rlopeslameira@gobarber',
-  expiresIn: '7d'
-};
--- dist/queue.js
+++
@@ -1,3 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _Queue = require('./lib/Queue'); var _Queue2 = _interopRequireDefault(_Queue);
-
-_Queue2.default.processQueue();
