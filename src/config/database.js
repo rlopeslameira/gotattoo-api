d5fc3e89de74b563f380339e47855f65c1a7f2f1
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-09 21:47:19 -0300

    ajust pm2 script

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   define: {
     timestamps: true,
