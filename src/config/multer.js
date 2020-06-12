7f4923044dd53cb655dc68fe75344e0d1f8ca813
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-11 22:27:01 -0300

    try catch

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,7 +4,8 @@ import { extname, resolve } from 'path';
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    // destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    destination: '/root/gotattoo-api/tmp/uploads/',
     filename: (req, file, cb) => {
       crypto.randomBytes(10, (err, res) => {
         if (err) return cb(err);
